18f8e7e0741c8619b5550020af7930942c512b21
Vajni <vajnork.zsolt@gmail.com>
2017-03-01 13:27:25 +0100

    Generated Table sceleton without Player class

---
+++ src/Table.java
@@ -0,0 +1,37 @@
+/**
+ * Created by vajni on 2017.03.01..
+ */
+public class Table implements Comparator {
+
+    List<Player> player;
+    byte numberOfPlayers;
+
+    public Table(byte numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+    }
+
+    public void setNumberOfPlayers(byte number){
+        this.numberOfPlayers = number;
+    }
+
+    public void addPlayersToTable(){
+        for (int i=0; i<numberOfPlayers; i++)
+            player.add();
+    }
+
+    public int compareByBoobSize(Player p1, Player p2) {
+
+    }
+
+    public int compareByAvailableHoles(Player p1, Player p2){
+
+    }
+
+    public int compareByCapacity(Player p1, Player p2) {
+
+    }
+
+    public int compareByPrice(Player p1, Player p2) {
+
+    }
+}
