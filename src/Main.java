7da23e3e4869e217c112e2e8117104859c976782
FerencOnodi <onodiferenc91@gmail.com>
2017-03-01 11:48:57 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ferenc on 2017.03.01..
+ */
+public class Main {
+    public static void main(String[] args) {
+        XMLParser xmlParser = new XMLParser();
+        xmlParser.XmlParser();
+    }
+}
---
+++ src/XMLParser.java
@@ -0,0 +1,48 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+/**
+ * Created by ferenc on 2017.03.01..
+ */
+public class XMLParser {
+
+    private static final String filePath = "src/Cards.xml";
+
+
+    public void XmlParser() {
+        try {
+            File inputFile = new File(filePath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+
+            doc.getDocumentElement().normalize();
+            System.out.println("Root Element: " + doc.getDocumentElement().getNodeName());
+            System.out.println("--------------");
+
+            NodeList nList = doc.getElementsByTagName("Card");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node node = nList.item(temp);
+                Element element = (Element) node;
+                System.out.println("\nName: " + element.getAttribute("name"));
+
+                System.out.println("Breast size: "
+                        + element.getElementsByTagName("breastSize").item(0).getTextContent());
+                System.out.println("Usable holes: "
+                        + element.getElementsByTagName("usableHoles").item(0).getTextContent());
+                System.out.println("Partner capacity: "
+                        + element.getElementsByTagName("partnerCapacity").item(0).getTextContent());
+                System.out.println("Price: "
+                        + element.getElementsByTagName("price").item(0).getTextContent());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
