b14d57e0365d93d1dbd71e8fbbb38057ccd70499
FerencOnodi <onodiferenc91@gmail.com>
2017-03-01 14:27:27 +0100

    Modified: XMLParser.java

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -1,5 +1,7 @@
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -12,6 +14,8 @@ import org.w3c.dom.Element;
 public class XMLParser {
 
     private static final String filePath = "src/Cards.xml";
+    private Map<String, Integer[]> cardData;
+    private Integer[] values = new Integer[4];
 
 
     public void XmlParser() {
@@ -27,9 +31,20 @@ public class XMLParser {
 
             NodeList nList = doc.getElementsByTagName("Card");
 
+            cardData = new HashMap<>();
+
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node node = nList.item(temp);
                 Element element = (Element) node;
+
+                values[0] = Integer.valueOf(element.getElementsByTagName("breastSize").item(0).getTextContent());
+                values[1] = Integer.valueOf(element.getElementsByTagName("usableHoles").item(0).getTextContent());
+                values[2] = Integer.valueOf(element.getElementsByTagName("partnerCapacity").item(0).getTextContent());
+                values[3] = Integer.valueOf(element.getElementsByTagName("price").item(0).getTextContent());
+
+                cardData.put(element.getAttribute("name"), values);
+
+
                 System.out.println("\nName: " + element.getAttribute("name"));
 
                 System.out.println("Breast size: "
@@ -44,5 +59,9 @@ public class XMLParser {
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public Map<String, Integer[]> getCardData() {
+        return cardData;
     }
 }
