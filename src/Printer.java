005f1ad430ed62a7b4b01d88c37a32c526be5509
Vajni <vajnork.zsolt@gmail.com>
2017-03-01 21:33:19 +0100

    Generated Printer.class

---
+++ src/Printer.java
@@ -0,0 +1,71 @@
+/**
+ * Created by Zsolt on 2017.03.01..
+ */
+public class Printer {
+
+    public void printNudeOne(){
+
+        System.out.println(",----------------------------------------------------------------------.-------------.");
+        System.out.println("|HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH|(C)Kroggscan |");
+        System.out.println("|HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH`-------------+");
+        System.out.println("|HHHHHHHHHHHHHHHHHHHHHHHHHHHHHF-.HHHHHHHHHF,,--,---`--`.:::::::::::::::::::/HHHHHHHHH|");
+        System.out.println("|HHHHHHHHHHHHHHHHHHHHHHHHHHHF/:::::::::::::::::::::::::::_.dHHHHHHHHb.:.:.:/HHHHHHHHH|");
+        System.out.println("|HHHHHHHHHHHHHHHHHHHHHHHHHHHj :.:.:.:.:.:.:.:.:.:.:.:.:.dHHHHHHHHHHHHHH : /HHHHHHHHHH|");
+        System.out.println("|HHHHHHHHHHHHHHHHHHHHHHHHHHH|.:.:.:.:.:.:.:.:.:.:.:.:.dHHHHHHHF```HHHHHH:JHP:`:`:HHHH|");
+        System.out.println("|HHHF': :`: :`:`: : : : `HHHF:.:.:.:.:.:.:.:.:.:.:.:.dHHHHHHF __  `HHHHHbF.:.:.: :.: |");
+        System.out.println("|HHF': : : : : : : : : : : :F::::::::::::_.----.::::dHHHHHH'   _`.    HHHH:.: :.:.:.:|");
+        System.out.println("|HF': : : : : : : : : : : : |::::::::;-'`       `::dHHHHHF    <j      HHHHb: :.: :.: |");
+        System.out.println("|F': : : : : : : : : : : : :j:::::;-`            |:HHHHHF         _`-.HHHHP.: :.: :.:|");
+        System.out.println("| : : : : : : : : : : : : : :/;--`                dHHHHF   ._  - /<jdHHHHF :.: : :.:.|");
+        System.out.println("|: : : : : : : : : : : :_.--`                      ```      ``-.`  dHHHF:F:.: :.: :.:|");
+        System.out.println("| :.: :.: : :.: :.: :,'`  ,             _.--'           ` .  `-'  dHHHF:j/.: :.:.:.:.|");
+        System.out.println("|: :.: :.: :.: :.: :/     `.      _,--'`                 ` `. _.dHHHH:::| `.:.: :.: :|");
+        System.out.println("|.: :.: :.: :.: :.: `       `--'``__               `     ,-dHHHHHHHHF:::F  / :.: :.:.|");
+        System.out.println("|:.:.:.:.:.:.:.:.:.:.`        ````  ```-.           ` .,dHHHHHHHHHHF:::j    F.: :.: :|");
+        System.out.println("|_:_:_:.:.:.:.:.:.:.:.:-.     `-.        `-.      - J  dHHHHHHHHHF`::::F    J:.:.:.:.|");
+        System.out.println("|:.:.:`:`:`:`:`:`:`--.-`/        `.         `-.  : oJ  |HHHHHHF``    ``-.    /:_:_:_:|");
+        System.out.println("|.:.:.:.:.:.:.:.:.: /  /         _ `-.         `-.`';  FHHHHHF                /.`.`.`|");
+        System.out.println("|:.:.:.:.:.:.:.:.:_/   |        ; `-._`.          `.`  jHHHHF_____            /:.:.:.|");
+        System.out.println("|.:.:.:.:.:.:.:.-' `.  | / /    |     ``            `-_;`HHF..:.:.:`:`:`-----'..:.:.:|");
+        System.out.println("|:.: :.: :.: :`     :;.|; ;  /\\ \\                       `.:.:.:.:.:.:.:.:.::.:.:.:.:.|");
+        System.out.println("|.:.: :.: :./       `'';-._,/. \\ \\                       `-.:.: :.: :.: :.: :.: :.: :|");
+        System.out.println("|:.: :.: :./          (:\\`\\`-`\\(_/                          `-_:.: :.: :.: :.: :.: :.|");
+        System.out.println("|.:.: :.: /            \\:\\`:.  \\_                              `..:.: :.: :.: :.: :.:|");
+        System.out.println("|: : : : j              ``::'   .:-._                            `-:_:.: : :.: : :.: |");
+        System.out.println("|.: : : :F                  ...:::'  ```-.._                         `-.: :.: : :.: :|");
+        System.out.println("|: : : :/                _ :::''     _,'::::::-._                       `-.: : : : : |");
+        System.out.println("|.: : :/                /       _,--`: : :`:`:`:::--._                      ``--:.: :|");
+        System.out.println("|: : :/                /   _.-:`: : : : : : : : : : : :-._           /            `-.|");
+        System.out.println("|.: ;/                /--:`: : : :       : : : : : : : : :`-._       \\               |");
+        System.out.println("`------------------------------------------------------------------------------------'");
+    }
+
+    public void printNudeTwo(){
+
+        System.out.println("                    _.._");
+        System.out.println("                .-'    -.");
+        System.out.println("                /  .-'` \\ `.");
+        System.out.println("                |   |    .-= `.");
+        System.out.println("                |   |_.-.\\` \\ '.");
+        System.out.println("                |   |\\  _.-; |  \\");
+        System.out.println("                |   | `\\`'' /   |");
+        System.out.println("               _|  .---.|``_..  |");
+        System.out.println("              /   /     `Y`   '.|\\");
+        System.out.println("             /   /       |      \\   `\\");
+        System.out.println("             |  /      . |.      \\ )  \\");
+        System.out.println("            /  /      |  | |      \\|   \\");
+        System.out.println("           /  /       /  | :       \\   |");
+        System.out.println("          /  /       /   |  \\       |   \\");
+        System.out.println("         /  /      /`    |   \\      \\   |");
+        System.out.println("        |  /     /`      |    \\      \\  |");
+        System.out.println("        |  `-. .-        |     `\\  .-'  )");
+        System.out.println("        |   -.\\          |       \\/.-/ /");
+        System.out.println("         \\  -`.\\        / \\      /`_///");
+        System.out.println("          |/`'-.|       | | )     |_.-' \\");
+        System.out.println("          /  `'-`        \\|/        \\-'` \\");
+        System.out.println("          |    |`-_       .       _.|     )");
+        System.out.println("          \\    |   `'-.._/ \\_..-'`  /    /");
+        System.out.println("           \\   |                   /    /");
+        System.out.println("            \\__|                  <____/");
+    }
+}
