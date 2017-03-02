663bc82f764b54bded10ed314ee89d1c43f36a7a
BiroMarton <rezoner9@gmail.com>
2017-03-02 14:52:50 +0100

    Modified: Main Table XMLParser

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,16 @@ public class Main {
     public static void main(String[] args) {
         Printer printer = new Printer();
 
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
         printer.print("Enter the number of players(2/3/4): ");
-        int number = scanner.nextInt();
-        Table t = new Table(number);
+        int number = scanner.nextInt();*/
+        Table t = new Table(2);
         t.parser.XmlParser();
         t.addPlayersToTable();
         t.handOutCards();
-        printer.printList(t.table.get(0).getCardsInHands());
-        printer.printList(t.table.get(1).getCardsInHands());
-        System.out.println(t.compareByAvailableHoles(t.table.get(0).getCardsInHands().get(0), t.table.get(1).getCardsInHands().get(0)));
+        //printer.printList(t.table.get(0).getCardsInHands());
+        //printer.printList(t.table.get(1).getCardsInHands());
+        t.game();
 
     }
 }
